345afa643f9ee03cee281d16f15b51fc93c8ab21
Your Name <gdikshan10@gmail.com>
2024-11-09 01:30:46 +0545

    sidbar links are implemented

--- src/components/shared/Sidebar.js
+++ src/components/shared/Sidebar.js
@@ -1,13 +1,44 @@
 import React from 'react'
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../lib/consts/navigation'
+import { Link, useLocation } from 'react-router-dom';
+import { HiOutlineLogout } from 'react-icons/hi';
 
+
+const linkClassesActive = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm font-bold text-white hover:text-white transition-all ease-in-out'
+const linkClasses = 'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base text-gray-300 hover:text-gray-300'
 const Sidebar = () => {
   return (
     <div className='flex flex-col bg-neutral-900 w-70 p-4 text-white'>
-        <div className='flex-1'>My Dashboard</div>
-        <div className='text-lg'>Open Shop</div>
-        <div className='text-red-600 font-semibold'>LogOut</div>
+        <div className='mb-3'>My Dashboard</div>
+        <div className='flex-1 flex flex-col gap-0.5 py-8'>
+            {DASHBOARD_SIDEBAR_LINKS.map((item)=>(
+              <SidebarLink key={item.key} item={item} />
+            ))}
+        </div>
+        <div className='border-t border-neutral-500 py-3'>
+          {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item)=>(
+            <div>
+              <SidebarLink key={item.key} item={item} />
+              <div className=''>
+                <span>
+                <HiOutlineLogout className='text-lg text-white hover:text-red-500 transition-all ease-in-out' />
+                </span>
+                Logout
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
 
-export default Sidebar
+function SidebarLink({item}){
+  const {pathname} = useLocation();
+  return (
+    <Link to={item.path} className={pathname === item.path ? linkClassesActive:linkClasses}>
+      <span>{item.icon}</span>
+      {item.label}
+    </Link>
+  )
+}
+export default Sidebar ;
