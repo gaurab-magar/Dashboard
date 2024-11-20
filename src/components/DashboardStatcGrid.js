74ac40ebecdbfe16913540ebe234b86a527e5321
gaurabMagar <gdikshan10@gmail.com>
2024-11-21 02:10:55 +0545

    dynamic dashBoardStatics Components implemented

--- src/components/DashboardStatcGrid.js
+++ src/components/DashboardStatcGrid.js
@@ -1,56 +1,27 @@
 import React from 'react'
 import {IoBagHandle} from 'react-icons/io5'
+import { DashboardStatcGridData } from '../lib/consts/DashboardStatcGridData'
 const DashboardStatcGrid = () => {
   return (
     <div className='flex gap-4'>
-        <BoxWrapper>
-            <div className='h-10 w-10 flex items-center justify-center bg-sky-500 rounded-full'>
-                <IoBagHandle className='text-2xl text-white' />
-            </div>
-            <div>
-                <h2 className='text-lg font-bold'>Total Sales</h2>
-                <div className='flex items-center gap-2'>
-                    <strong className='text-gray-500 font-semibold'>$2346.4360</strong>
-                    <p className='text-sm text-sky-500'>+235</p>
-                </div>
-            </div>
-        </BoxWrapper>
-        <BoxWrapper>
-            <div className='h-10 w-10 flex items-center justify-center bg-red-500 rounded-full'>
-                <IoBagHandle className='text-2xl text-white' />
-            </div>
-            <div>
-                <h2 className='text-lg font-bold'>Total Expenses</h2>
-                <div className='flex items-center gap-2'>
-                    <strong className='text-gray-500 font-semibold'>$2346.4360</strong>
-                    <p className='text-sm text-sky-500'>+235</p>
-                </div>
-            </div>
-        </BoxWrapper>
-        <BoxWrapper>
-            <div className='h-10 w-10 flex items-center justify-center bg-yellow-500 rounded-full'>
-                <IoBagHandle className='text-2xl text-white' />
-            </div>
-            <div>
-                <h2 className='text-lg font-bold'>Total Customers</h2>
-                <div className='flex items-center gap-2'>
-                    <strong className='text-gray-500 font-semibold'>$2346.4360</strong>
-                    <p className='text-sm text-sky-500'>+235</p>
-                </div>
-            </div>
-        </BoxWrapper>
-        <BoxWrapper>
-            <div className='h-10 w-10 flex items-center justify-center bg-green-500 rounded-full'>
-                <IoBagHandle className='text-2xl text-white' />
-            </div>
-            <div>
-                <h2 className='text-lg font-bold'>Total Orders</h2>
-                <div className='flex items-center gap-2'>
-                    <strong className='text-gray-500 font-semibold'>$2346.4360</strong>
-                    <p className='text-sm text-sky-500'>+235</p>
-                </div>
-            </div>
-        </BoxWrapper>
+        {
+            DashboardStatcGridData.map((item)=>{
+                return (
+                        <BoxWrapper key={item.id}>
+                            <div className={`h-10 w-10 flex items-center justify-center ${item.color} rounded-full`}>
+                                <IoBagHandle className='text-2xl text-white' />
+                            </div>
+                            <div>
+                                <h2 className='text-lg font-bold'>{item.title}</h2>
+                                <div className='flex items-center gap-2'>
+                                    <strong className='text-gray-500 font-semibold'>${item.value}</strong>
+                                    <p className='text-sm text-sky-500'>{item.percentage}%</p>
+                                </div>
+                            </div>
+                        </BoxWrapper>
+                )
+            })
+        }      
     </div>
   )
 }
